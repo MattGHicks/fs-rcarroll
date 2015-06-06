794526aae828052b535b96f54e8427ff208bb092
Matt <matthicks@fullsail.edu>
2015-06-06 00:20:55 -0400

    Added my IF statements

--- Madlib/madlib.py
+++ Madlib/madlib.py
@@ -1,18 +1,27 @@
+print ""
+print ""
+
 #Input Variables
-print ""
-print ""
+
 nick_name = raw_input("Give us your nick name:  ")
 weapon = raw_input("Name a deadly weapon of choice:  ")
 faction = raw_input("Name your faction:  ")
 homeland = raw_input("What is the name of your homeland:  ")
 minute1 = int(raw_input("Enter a number 1-5:  "))
 minute2 = int(raw_input("Enter a number 1-3:  "))
-born = raw_input("What year were you born")
+born = int(raw_input("What year were you born:  "))
+odd_or_even = raw_input("Pick 'odd' or 'even':  ")
+rock_or_rap = raw_input("Which do you like better? 'rock' or 'rap':  ")
 
-#Declared Variables
+#Array
 
+allies = ["Bohemoths" , "Grunts" , "Redshirts" , "Scallywags"]
+
+artists = dict()
+artists = {"rap":"Biggie Smalls", "rock":"Rage Against The Machine"}
 
 #Print Statements
+
 print ""
 print "*-*-*-*-*-*-*-*- YEAR 2943 - BATTLE OF THE ROBOTS *-*-*-*-*-*-*-*- "
 print ""
@@ -22,7 +31,35 @@ print "I pray that you have brought your trusty " + weapon + " with you."
 print ""
 print "The robots are spreading through the universe at a rapid pace and "
 print "will soon take over your beloved " + homeland + ". You only have "
-print "approximately " + str(minute1+minute2 ) + " minutes."
+print "approximately " + str(minute1*minute2 ) + " minutes."
 print ""
 print "Seeming as you are " + str(2943-born) + " years old, you must use "
 print "your wisdom against the new generation of deadly robot machines."
+
+#If Statements
+
+if odd_or_even == str("odd"): 
+	picked_allies = allies[0] + " and " + allies[2]
+	print "Today you will fight with your allies " + picked_allies + "."
+	print ""
+
+else:
+	picked_allies = allies[1] + " and " + allies[3]
+	print "Today you will fight with your allies " + picked_allies + "."
+	print ""
+
+if rock_or_rap == str("rap"):
+	print "As your soaring through the galaxy straight for the robot hive, "
+	print artists["rap"] + " begins to play on the radio."
+
+elif rock_or_rap == str("rock"):
+	print "As your soaring through the galaxy straight for the robot hive, "
+	print artists["rock"] + " begins to play on the radio."	
+else:
+	print "You concentrate in silence while approaching the colony."
+
+
+
+
+
+
